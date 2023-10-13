700513a67de410ebba4520f811c52e9e91fffc62
caz <cazpfitl@gmail.com>
2023-10-13 01:24:25 -0600

    v0.4.31

--- src/js/Snake2/src/Controls.js
+++ src/js/Snake2/src/Controls.js
@@ -3,6 +3,9 @@ class Controls {
     reverse = 0
     right = 0
     left = 0
+    touchStartX = 0;
+    touchStartY = 0;
+    sensitivityThreshold = 20;
 
     /**
      * Create a new Controls instance to manage movement controls.
@@ -10,9 +13,8 @@ class Controls {
      * @param {App} app - The application instance.
      * @param {Entity} entity - The entity for which controls are applied.
      */
-    constructor(app, entity) {
+    constructor(app) {
         this.app = app
-        this.entity = entity
         const props = Object.getOwnPropertyNames(this);
         const eventTypes = props.filter(prop => typeof this[prop] === 'function');
         eventTypes.forEach(eventType => {
@@ -81,30 +83,20 @@ class Controls {
         const deltaX = touch.clientX - this.touchStartX;
         const deltaY = touch.clientY - this.touchStartY;
 
-        // Implement your logic for movement based on deltaX and deltaY.
-        // You can use thresholds or other conditions to determine the direction.
-
-        // Example logic:
-        if (deltaX > 10) {
-            this.right = 1;
-            this.left = 0;
-        } else if (deltaX < -10) {
-            this.left = 1;
-            this.right = 0;
+        if (Math.abs(deltaX) > this.sensitivityThreshold) {
+            this.right = deltaX > 0 ? 1 : 0;
+            this.left = deltaX < 0 ? 1 : 0;
         } else {
             this.right = 0;
             this.left = 0;
         }
 
-        if (deltaY > 10) {
-            this.reverse = 1;
-            this.forward = 0;
-        } else if (deltaY < -10) {
-            this.forward = 1;
-            this.reverse = 0;
+        if (Math.abs(deltaY) > this.sensitivityThreshold) {
+            this.reverse = deltaY > 0 ? 1 : 0;
+            this.forward = deltaY < 0 ? 1 : 0;
         } else {
-            this.forward = 0;
             this.reverse = 0;
+            this.forward = 0;
         }
     }
 
